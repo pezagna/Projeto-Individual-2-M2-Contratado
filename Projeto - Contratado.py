16b27516efd1b8af2f2d876ba8205ad8599f4100
Rob <123372510+pezagna@users.noreply.github.com>
2023-02-16 21:23:33 -0300

    Atualização de textos e comentários.

--- Projeto - Contratado.py
+++ Projeto - Contratado.py
@@ -1,5 +1,5 @@
 ##################################################################
-#  Projeto individual 2 - Módulo 2 - Contratando!                #
+#  Projeto individual 2 - Módulo 2 - Contratado!                 #
 #  Data de criação: 14/02/2023                                   #
 #  Criação e edição:  Robson da Silva                            #
 #  versão = '3.11' 64 bit                                        #
@@ -24,11 +24,11 @@ keywords2 = "Ánalise de dados" or "ánalise de dados" and "Dados" or "dados" an
 vaga2 = {}
 aproved2 = {}
 notaproved2 = {}
-def cadastro():
+def cadastro():#Função onde será realizado o cadastro para a vaga 1, se aprovado o candidato será enviado para o dicionário aproved, caso contrário, será movido para o notaproved
     continuar = 1
     while continuar !=0:
-        nome = input("Digite o nome do candidato: ")
-        curriculo = input("Envie o currículo do candidato: ")
+        nome = str.title(input("Digite o nome do candidato: "))
+        curriculo = str.title(input("Envie o currículo do candidato: "))
         vaga1.update({nome:curriculo})
         for chave,valor in vaga1.items():
             if keywords in valor:
@@ -38,12 +38,12 @@ def cadastro():
                 print(f"\nCandidato {chave} não está qualificado para a vaga.")
                 notaproved.update({nome:curriculo})
         continuar = int(input("\nDigite 1 para continuar. Digitte 0 para encerrar: "))
-    for chave,valor in aproved.items():
-        print(f"\nCandidato aprovado:\nNome: {chave}\nCurrículo: {valor}\n")
-    for chave,valor in notaproved.items():
-        print(f"Candidato não aprovado: \nNome: {chave}\nCurrículo: {valor}\n")
+        for chave,valor in aproved.items():
+            print(f"\nCandidato aprovado:\nNome: {chave}\nCurrículo: {valor}\n")
+        for chave,valor in notaproved.items():
+            print(f"Candidato não aprovado: \nNome: {chave}\nCurrículo: {valor}\n")
 
-def cadastro2():
+def cadastro2():#Função onde será realizado o cadastro para a vaga 2, se aprovado o candidato será enviado para o dicionário aproved, caso contrário, será movido para o notaproved
     continuar = 1
     while continuar !=0:
         nome = input("Digite o nome do candidato: ")
@@ -62,7 +62,7 @@ def cadastro2():
     for chave,valor in notaproved2.items():
         print(f"Candidato não aprovado: \nNome: {chave}\nCurrículo: {valor}\n")
     
-def menuvaga1():
+def menuvaga1():#Submenu para acessar o cadastramento e listar os candidatos aprovados apenas para a vaga 1 
     opcao=0
     while opcao !="3":
         print("\n""1. Cadastrar currículo""\n""2. Listar candidatos" "\n""3. Voltar" "\n")
@@ -83,7 +83,7 @@ def menuvaga1():
         else:
             print ("\n""Opcão inválida, tente novamente.")
             
-def menuvaga2():
+def menuvaga2():#Submenu para acessar o cadastramento e listar os candidatos aprovados apenas para a vaga 2
     opcao=0
     while opcao !="3":
         print("\n""1. Cadastrar currículo""\n""2. Listar candidatos" "\n""3. Voltar" "\n")
@@ -104,7 +104,7 @@ def menuvaga2():
         else:
             print ("\n""Opcão inválida, tente novamente.")
 
-def menu(): #Menu Principal
+def menu():#Menu Principal e tela inicial da aplicação. O usuário poderá selecionar para qual vaga irá cadastrar os currículos e também listar todos os candidatos qualificados.
     opcao=0
     while opcao !="3":
         print("\n     Selecione a vaga desejada: ""\n""\n""1. Vaga 1: Python, Programação, Desenvolvimento""\n""2. Vaga 2: Análise de dados, Dados, SQL" "\n""3. Listar todos os candidatos aprovados" "\n""4. Sair")
